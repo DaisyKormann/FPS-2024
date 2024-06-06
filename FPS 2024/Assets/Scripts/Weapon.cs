d9befa0c55b3d33a4484f749bec516425115e0fa
DaisyKormann <161627745+DaisyKormann@users.noreply.github.com>
2024-06-06 09:07:00 -0300

    Update Weapon.cs

--- FPS 2024/Assets/Scripts/Weapon.cs
+++ FPS 2024/Assets/Scripts/Weapon.cs
@@ -1,17 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] WeaponModel weapon;
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletImpact;
+
+
     void Start()
+    {
+        GetComponentInChildren<MeshFilter>();
+        GetComponentInChildren<MeshRenderer>();
+
+        // Inicialize o carregador atual com a capacidade máxima do carregador da arma
+
+        // Configure a aparência da arma
+    }
+
+    void Update()
     {
         
     }
+    void Fire()
+    {
+        StartCoroutine("Disparo Arma");
+    }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FireCoroutine()
+    {
+        
+        // Verifica se pode disparar
+
+        // Reduz a munição do carregador
+        // Define o tempo para o próximo disparo
+
+        // Dispara projéteis com o tempo entre cada disparo
+    }
+
+    private void Shoot()
+    {
+
+    }
+
+    void Reload()
+    {
+
+    }
+
+    private IEnumerator ReloadCoroutine()
+    {
+
+    }
+
+    private void OnDrawGizmos()
     {
         
     }
