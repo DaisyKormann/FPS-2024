287872460f46472a8a72f6aaa60e36b4ddf7a2f4
DaisyKormann <161627745+DaisyKormann@users.noreply.github.com>
2024-06-13 09:57:36 -0300

    novos

--- FPS 2024/Assets/Scripts/Weapon.cs
+++ FPS 2024/Assets/Scripts/Weapon.cs
@@ -1,25 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] WeaponModel weapon;
+    [SerializeField] WeaponModel weaponModel;
     [SerializeField] Transform firePoint;
     [SerializeField] GameObject bulletImpact;
-    [SerializeField] int magazine;
 
+     MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+    int currentMagazine;
+
+    float timeToShoot;
 
     void Start()
     {
 
-        GetComponentInChildren<MeshFilter>();
-        GetComponentInChildren<MeshRenderer>();
+        meshFilter = GetComponentInChildren<MeshFilter>();
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
 
-        magazine = weapon.MagazineCap; // Inicialize o carregador atual com a capacidade máxima do carregador da arma
+        currentMagazine = weaponModel.MagazineCap; // Inicialize o carregador atual com a capacidade máxima do carregador da arma
 
-       //? Configure a aparência da arma 
+       // Configure a aparência da arma ja feito acima
 
     }
 
@@ -29,38 +36,54 @@ public class Weapon : MonoBehaviour
     }
     void Fire()
     {
-        StartCoroutine("Disparo Arma");
+        StartCoroutine(FireCoroutine()); // chamar um método
     }
     
-    private IEnumerator FireCoroutine()
+    IEnumerator FireCoroutine()
     {
-
-        if () // Verifica se pode disparar
+        // Verifica se pode disparar
+        if (Time.time >= timeToShoot) // verificar munição e fire rate
         {
-            magazine--;         // Reduz a munição do carregador
-            weapon.FireRate = 7;         // Define o tempo para o próximo disparo
+
+            // Define o tempo para o próximo disparo
+            timeToShoot = Time.time + 1 / weaponModel.FireRate;
+
+            // Dispara projéteis com o tempo entre cada disparo
+            if (int i = 0; i < weaponModel.BulletsForShoot; i++)
+            {
+                if (currentMagazine > 0)
+                {
+                    // Reduz a munição do carregador
+                    currentMagazine--;
+                    Shoot();
+                    yield return new WaitForSeconds(weaponModel.TimeBetweenShoot);
+                }
+            }
 
         }
-
-
-
-        // Dispara projéteis com o tempo entre cada disparo
 
     }
 
     private void Shoot()
     {
         // Variável para armazenar o que foi atingido
+        RaycastHit hit;
+
         // Direção do disparo, considerando a dispersão da arma
+        Vector3 dirction = new Vector3(Random.Range(-weaponModel.Spread, weaponModel.Spread), Random.Range(-weaponModel.Spread, weaponModel .Spread, 1));
 
         // Verifica se acertou algo na direção do disparo dentro do alcance
+        if (Physics.Raycast(firePoint.position, out hit, weaponModel.Range))
+        {
+
+        }
 
         // Desenha uma linha para visualizar o trajeto do projétil
     }
 
     void Reload()
     {
-        StartCoroutine("Recarga");
+        StartCoroutine(ReloadCoroutine());
     }
 
     private IEnumerator ReloadCoroutine()
--- FPS 2024/Assets/Scripts/WeaponModel.cs
+++ FPS 2024/Assets/Scripts/WeaponModel.cs
@@ -8,7 +8,7 @@ public class WeaponModel : ScriptableObject
 {
     [SerializeField] float damage;
     [SerializeField] float range;
-    [SerializeField] float fireRate;
+    public [SerializeField] float fireRate;
     [SerializeField] float spread;
     [SerializeField] float reloadTime;
     [SerializeField] float timeBetweenShoot;
@@ -20,19 +20,19 @@ public class WeaponModel : ScriptableObject
     [SerializeField] Material material;
     [SerializeField] Vector3 weaponPosition;
 
-    public float Damage { get => damage; set => damage = value; }
-    public float Range { get => range; set => range = value; }
-    public float FireRate { get => fireRate; set => fireRate = value; }
-    public float Spread { get => spread; set => spread = value; }
-    public float ReloadTime { get => reloadTime; set => reloadTime = value; }
-    public float TimeBetweenShoot { get => timeBetweenShoot; set => timeBetweenShoot = value; }
-    public int MagazineCap { get => magazineCap; set => magazineCap = value; }
-    public int BulletsForShoot { get => bulletsForShoot; set => bulletsForShoot = value; }
-    public bool Automatic { get => automatic; set => automatic = value; }
-    public bool Scope { get => scope; set => scope = value; }
-    public Mesh Model { get => model; set => model = value; }
-    public Material Material { get => material; set => material = value; }
-    public Vector3 WeaponPosition { get => weaponPosition; set => weaponPosition = value; }
+    public float Damage { get => damage;}
+    public float Range { get => range; }
+    public float FireRate { get => fireRate; }
+    public float Spread { get => spread;}
+    public float ReloadTime { get => reloadTime; }
+    public float TimeBetweenShoot { get => timeBetweenShoot;  }
+    public int MagazineCap { get => magazineCap;  }
+    public int BulletsForShoot { get => bulletsForShoot;  }
+    public bool Automatic { get => automatic;  }
+    public bool Scope { get => scope; }
+    public Mesh Model { get => model;  }
+    public Material Material { get => material;  }
+    public Vector3 WeaponPosition { get => weaponPosition;  }
 
     // Start is called before the first frame update
     void Start()
